f1503c808525acf02f0ec628c0ab11b3785245cc
IndiraBriggiler <ibriggiler@gmail.com>
2023-02-13 01:24:17 +0100

    Hex Viewer

--- hex-editor-challenge-react-ts/app/src/App.tsx
+++ hex-editor-challenge-react-ts/app/src/App.tsx
@@ -8,35 +8,40 @@ function App() {
     const result = await readFile(e);
     setFile(result);
   }
-
-  const renderComponents = () => {
-    if (!file) {
-      return (
-        <input
-          name="file"
-          type="file"
-          role="button"
-          onInput={updateFileState}
-        />
-      )
-    }
-
-    const isBinary = typeof file !== 'string';
-    return (
-      <>
-        <div>
-          <span>Loaded {isBinary ? 'binary' : 'text'} file</span>
-          {' '}
-          <button onClick={() => setFile(null)}>Reset</button>
-        </div>
-        <HexViewer data={file} />
-      </>
-    )
-  };
+  const [onHover, setOnHover] = React.useState(false);
+  const toggleHover = () => setOnHover(!onHover);
+  const isBinary = typeof file !== 'string';
 
   return (
     <div className="App">
-      {renderComponents()}
+      <header id="header" onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+        <a target="_blank" href="https://www.fiskaly.com/">
+          <figure className="logoContainer">
+            <img src="assets/img/logoBlack.svg" alt="Fiskaly logo" className={onHover ? '' : 'hidden'}
+            />
+            <img src="assets/img/logoWhite.svg" alt="Fiskaly logo" className={onHover ? 'hidden' : ''}
+            />
+          </figure>
+        </a>
+      </header>
+
+      <section className='titleContainer'>
+        <span className='title'>Hex Viewer</span>
+        <div className='divider'></div>
+        <div className='actionsContainer'>
+          {!file ?
+            (<input
+              name="file"
+              type="file"
+              role="button"
+              onInput={updateFileState}
+            />) : (
+              <div className='resetContainer'>
+                <span>Loaded <span className='bold'> {isBinary ? 'binary' : 'text'}</span> file</span>
+                <button className='button' onClick={() => setFile(null)}>Reset</button></div>
+            )}</div>
+      </section>
+      {file && <HexViewer data={file} offset={0} />}
     </div>
   );
 }
--- hex-editor-challenge-react-ts/app/src/components/HexViewer.tsx
+++ hex-editor-challenge-react-ts/app/src/components/HexViewer.tsx
@@ -1,17 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface HexViewerProps {
   data: string | Uint8Array;
+  offset: number;
 }
 
-export default function HexViewer(props: HexViewerProps) {
-  /*
-   * This component is the main challenge. You can be wild here and change
-   * everything!
-   */
+export default function HexViewer({ data, offset: startOffset }: HexViewerProps) {
+
+  const lines: React.ReactElement[] = [];
+  const [cursor, setCursor] = useState<number>();
+  const BYTES_PER_LINE = 16;
+  const toHex = (n: number, length: number) => n.toString(16).padStart(length, '0');
+
+  if (typeof data === 'string') {
+    const encoder = new TextEncoder();
+    data = encoder.encode(data);
+  }
+
+  for (let lineNumber = 0; lineNumber < data.length / BYTES_PER_LINE; lineNumber++) {
+
+    const offset = startOffset + (lineNumber * BYTES_PER_LINE);
+
+    const slice = Array.from(data.slice(offset, offset + BYTES_PER_LINE));
+
+    const offsetComponent = <span>{toHex(offset, 8)}: </span>;
+
+    const onSelectedItem = (offset: number, value: string) => {
+      setCursor(offset);
+      navigator.clipboard.writeText(value);
+    }
+
+    const bytes = slice.map((byte, i) => {
+      const spacing = ((i + 1) % 4 === 0) ? '  ' : ' ';
+      const hex = toHex(byte, 2);
+      if (offset + i === cursor) {
+        return <div key={offset + i} >
+          <span className="highlight">{hex}</span>
+          <span>{spacing}</span>
+        </div>;
+      }
+
+      return <span key={offset + i} onClick={() => onSelectedItem(offset + i, hex)}>{hex}{spacing}</span>;
+
+    });
+
+    if (bytes.length < BYTES_PER_LINE) {
+      const wordSpaces = 4 - Math.floor(bytes.length / 4);
+      const diff = BYTES_PER_LINE - bytes.length;
+      const padding = <span key='padding'>{'   '.repeat(diff)}{' '.repeat(wordSpaces)}</span>;
+      bytes.push(padding);
+    }
+
+    const bytesComponent = <div className='bytesComponent'>{bytes}</div>;
+    const asciiComponent = <div className='asciiComponent'>
+      {slice.map((byte, i) => {
+        const char = (byte >= 0x20 && byte < 0x7f)
+          ? String.fromCharCode(byte)
+          : '.';
+
+        if (offset + i === cursor) {
+          return <span className="highlight" key={char + i}>{char}</span>
+        }
+
+        return <span onClick={() => onSelectedItem(offset + i, char)} key={char + i}>
+          {char}
+        </span>
+      })}
+      {slice.length < BYTES_PER_LINE && <span>
+        {''.padStart(BYTES_PER_LINE - slice.length, ' ')}
+      </span>}
+    </div>;
+
+    lines.push(<div key={offset} className="lines" > {offsetComponent}{bytesComponent} {asciiComponent}</div >);
+  }
+
   return (
-    <pre style={{ overflowWrap: 'break-word', whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
-      Here comes the HexViewer<br />{ props.data }
+    <pre className='dataContainer' >
+      <div className='linesContainer'>{lines}</div>
     </pre>
   );
 }
